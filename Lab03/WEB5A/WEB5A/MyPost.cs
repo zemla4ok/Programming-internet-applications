a7eb337e26ea20d270686bf19ba550ca122bf390
zemla4ok <dima-vital@live.com>
2018-03-06 10:00:41 +0300

    Complete LAB02.WEB5A

---
+++ Lab03/WEB5A/WEB5A/MyPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB5A
+{
+    public class MyPost : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest req = context.Request;
+            HttpResponse res = context.Response;
+
+            int x = Convert.ToInt32(req.Headers["X-Value-x"]);
+            int y = Convert.ToInt32(req.Headers["X-Value-y"]);
+
+            res.Headers.Add("X-Value-z", (x + y).ToString());
+        }
+    }
+}
