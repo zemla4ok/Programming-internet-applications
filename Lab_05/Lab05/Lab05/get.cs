27e94dae43623c835102ccad7d29c6306f533065
zemla4ok <dima-vital@live.com>
2018-04-03 09:55:52 +0300

    complete lab5

---
+++ Lab_05/Lab05/Lab05/get.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Lab05
+{
+    public class get : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string head = context.Request.Headers["Authorization"];
+            if (head != null)
+            {
+                var base64Encoded = head.Replace("Basic ", "");
+                string information = Encoding.UTF8.GetString(Convert.FromBase64String(base64Encoded));
+                string[] i = information.Split(':');
+                string login = i[0];
+                string password = i[1];
+                context.Response.StatusCode = 200;
+                context.Response.Write($"login: {login} <br> password: {password}");
+            }
+            else
+            {
+                context.Response.Headers.Add("WWW-Authenticate", "Basic");
+                context.Response.StatusCode = 401;
+                context.Response.End();
+            }
+        }
+    }
+}
